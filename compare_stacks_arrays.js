0954fe6d6d23aee489410324edfd83bd82f84f7a
zkouba <zkouba@me.com>
2017-01-12 07:33:24 -0800

    revision 1

--- compare_stacks_arrays.js
+++ compare_stacks_arrays.js
@@ -1,5 +1,5 @@
 function compare_stacks(arr1,arr2){
-  if arr1.length != arr2.length{
+  if(arr1.length != arr2.length){
   	return false
   }
   for(var i = 0; i < arr1.length; i++){
@@ -9,3 +9,19 @@ function compare_stacks(arr1,arr2){
   }
   return true
 }
+
+
+var arr1 = [1, 2, 3, 4, 5, 6]
+var arr2 = [1, 2, 3, 4, 5, 6]
+var test1 = compare_stacks(arr1, arr2)
+console.log(test1)
+
+var arr3 = [1, 2, 3, 4, 5, 6]
+var arr4 = [1, 2, 3, 4, 6, 6]
+var test2 = compare_stacks(arr3, arr4)
+console.log(test2)
+
+arr5 = [1, 2, 3, 4, 5, 6]
+arr6 = [1, 2, 3, 4, 5]
+var test3 = compare_stacks(arr5, arr6)
+console.log(test3)
--- compare_stacks_linked_lists.js
+++ compare_stacks_linked_lists.js
@@ -1,28 +1,118 @@
-function Node(val){
-  this.val = val;
+function Node(value){
+  this.val = value;
   this.next = null;
 }
 function SLL(){
-  this.head = null
+	this.head = null
+	this.tail = null
+	this.push = function(node){	
+		if(!this.head){
+			this.head = node
+			this.tail = node
+			return this
+		}
+		else{
+			this.tail.next = node
+			this.tail = this.tail.next
+			return this
+		}
+	}
+	this.pop = function(){
+		if(!this.head){
+			return this
+		}
+		if(!this.head.next){
+			this.head = null
+		}
+		curr = this.head
+		while(curr.next.next){
+			curr = curr.next
+		}
+		curr.next = null
+		return this
+	}
+	this.compare_stacks = function compare_stacks(sll){
+  		if(!this.head && !sll.head){
+  			return true
+ 		 }
+  		if(!this.head || !sll.head){
+  			return false
+  		}
+	  if(this.head.val !== sll.head.val){
+	  	return false
+	  }
+	  var curr1 = this.head
+	  var curr2 = sll.head
+	  while(curr1.next || curr2.next){
+	  	if(!curr1.next || !curr2.next){
+	  		return false
+	  	}
+	  	else if(curr1.next.val !== curr2.next.val){
+	  		return false
+	  	}
+	  	curr1 = curr1.next
+	  	curr2 = curr2.next
+	  }
+	  return true 
+	}
 }
-function compare_stacks(arr1,arr2){
-  if(!arr1.head && !arr2.head){
-  	return true
-  }
-  if(!arr1.head || !arr2.head){
-  	return false
-  }
-  if(arr1.head.val !== arr2.head.val){
-  	return false
-  }
-  var curr1 = arr1.head
-  var curr2 = arr2.head
-  while(curr1.next || curr2.next){
-  	if(curr1.next.val !== curr2.next.val){
-  		return false
-  	}
-  	curr1 = curr1.next
-  	curr2 = curr2.next
-  }
-  return true 
-}
+
+
+
+var list1 = new SLL()
+var node1 = new Node(value = 1)
+list1.push(node1)
+var node2 = new Node(2)
+list1.push(node2)
+var node3 = new Node(3)
+list1.push(node3)
+var node4 = new Node(4)
+list1.push(node4)
+// end of first list & nodes
+var list2 = new SLL()
+var node5 = new Node(1)
+list2.push(node5)
+var node6 = new Node(2)
+list2.push(node6)
+var node7 = new Node(3)
+list2.push(node7)
+var node8 = new Node(4)
+list2.push(node8)
+// end of second list & nodes
+var list3 = new SLL()
+var node9 = new Node(1)
+list3.push(node9)
+var node10 = new Node(2)
+list3.push(node10)
+var node11 = new Node(3)
+list3.push(node11)
+var node12 = new Node(4)
+list3.push(node12)
+var node13 = new Node(13)
+list3.push(node13)
+// end of third list & nodes
+var list4 = new SLL()
+// end of fourth list
+var list5 = new SLL()
+// end of fifth list
+var test1 = list1.compare_stacks(list2)
+console.log("Below should be True")
+console.log(test1)
+var test2 = list1.compare_stacks(list3)
+console.log("Below should be false")
+console.log(test2)
+var test3 = list1.compare_stacks(list4)
+console.log("Below should be False")
+console.log(test3)
+var test4 = list4.compare_stacks(list5)
+console.log("Below should be True")
+console.log(test4)
+
+
+
+
+
+
+
+
+
